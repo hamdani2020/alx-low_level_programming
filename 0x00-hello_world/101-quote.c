8ff76e217350283b3461b6e03d15b9684d5e37ba
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-02-16 18:09:57 -0800

    mm

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print the size of various types
+ *
+ * Description: prints the size of various types on the computer it is compiled
+ * and run on
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() {
+  const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(2, message, 42);
+  return 1;
+}
