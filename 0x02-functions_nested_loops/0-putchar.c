bbd8cb4f1260c67a032b0863447696663771e5ab
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-02-21 14:30:15 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints Holberton
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
