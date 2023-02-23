e4b88715609c7647d1b7270f2ffbd9e10e5c62f8
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-02-23 13:46:59 -0800

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,6 @@
 
 /**
  * main - causes an infinite loop
- *
  * Return: 0
  */
 
@@ -18,7 +17,7 @@ int main(void)
 	/*{*/
 	/*putchar(i);*/
 	/*}*/
-	
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
