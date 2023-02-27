8704cf7bf5479092a3bb47ee3ab3cd14ab241c2a
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-02-27 13:09:03 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * prints a string, followed by a new line
+ *@str: input string.
+ *
+ *Return: 0
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+
+		_putchar(str[count]);
+		count++;
+	}
+}
