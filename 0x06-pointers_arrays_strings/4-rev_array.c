87281f7536470caa81d8b55615e050b3e7443a0d
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-03-02 17:52:54 -0800

    challenge 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,62 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * reverse_array - reverses the contents of an array
+ * @author: muhabeid
+ * @a: integer array to be reversed
+ * @n: length of array a
  *
  * Return: void
  */
-
-void print_line(char *c, int s, int l)
+void reverse_array(int *a, int n)
 {
-	int j, k;
+	int i = 0, t;
 
-	for (j = 0; j <= 9; j++)
+	n = n - 1;
+	while (i < n)
 	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
+		t = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = t;
+		i++;
+		n--;
 	}
 }
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- *
- * Return: void
- */
-void print_buffer(char *b, int size)
-{
-	int i;
-
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
-	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
-		{
-			print_line(b, 9, i);
-		}
-		else
-		{
-			print_line(b, size % 10 - 1, i);
-		}
-		putchar('\n');
-	}
-	if (size == 0)
-		putchar('\n');
-}
-
