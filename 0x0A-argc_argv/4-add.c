a42f3483c4cc58ba4edfca56bd86a61abb6160bb
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-03-10 13:58:00 -0800

    5th challenge

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,8 +16,46 @@ int isNum(char num[])
 
 	for (i = 0; i < l; i++)
 	{
-		if(!isdigit(num[i]))
+		if (!isdigit(num[i]))
 			return (1);
+	}
+	return (0);
+}
+
+
+/**
+ * main - a program that adds positive numbers
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			if (isNum(argv[i]) == 0)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
