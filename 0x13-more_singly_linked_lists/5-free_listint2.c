77896350f1e3cf04f13dd8558d48cb246951edcb
Alhassan Hamdani Gandi <hamdanialhassangandi2020@gmail.com>
2023-04-04 16:01:09 -0700

    fix bug, for realz

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,17 +10,14 @@
 
 void free_listint2(listint_t **head)
 {
-	if (head == NULL || *head == NULL) {
+	listint_t *next;
+
+	if (head == NULL)
 		return;
+	while (*head)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
 	}
-
-	listint_t *current = *head;
-	while (current != NULL) {
-		listint_t *next = current->next;
-		free(current);
-		current = next;
-	}
-
-	*head = NULL;
 }
-
